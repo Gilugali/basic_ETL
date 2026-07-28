946c8814b5830684ee803168ba58d2f28ad49632
Juste <mugishajuste1@gmail.com>
2026-07-27 23:05:01 -0400

    [ft: extractor, ingestion and error logs]

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+        var rand = new Random();
+
+        // var symbols = new List<string> { "AAPL", "MSFT", "AMZN", "TSLA", "GOOG" };
+        // var priceData = new List<PriceData>();
+
+        // foreach (var symbol in symbols)
+        // {
+        //     decimal closePrice = 0;
+        //     var date = new DateOnly(2025, 12, 20);
+        //     const int year = 2025;
+
+        //     decimal open = rand.Next(10, 500);
+        //     closePrice = open;
+
+        //     date = SkipWeekends(date);
+        //     while (date.Year == year)
+        //     {
+        //         open = closePrice + ((decimal)rand.NextDouble() - (decimal)rand.NextDouble()) * 4;
+        //         closePrice = open + ((decimal)rand.NextDouble() - (decimal)rand.NextDouble()) * 4;
+        //         var high = Math.Max(open, closePrice);
+        //         high += (decimal)rand.NextDouble() * 4;
+        //         var low = Math.Min(open, closePrice);
+        //         low -= (decimal)rand.NextDouble() * 4;
+        //         var volume = rand.Next(100000, 10000000);
+
+        //         var prices = new PriceData(symbol, date, open, high, low, closePrice, volume);
+        //         priceData.Add(prices);
+        //         date = date.AddDays(1);
+        //         date = SkipWeekends(date);
+        //     }
+        // }
+
+        // var sortedData = priceData.OrderBy(o => o.Symbol).ThenBy(d => d.Date).ToList();
+        // var lines = new List<string> { "symbol,date,open,high,low,close,volume" };
+
+        // foreach (var (symbol, date, open, high, low, close, volume) in sortedData)
+        // {
+        //     lines.Add($"{symbol},{date:MM/dd/yyyy},{open:F2},{high:F2},{low:F2},{close:F2},{volume}");
+        // }
+
+        // File.WriteAllLines("prices.csv", lines);
+        // Console.WriteLine($"Wrote {lines.Count - 1} rows to prices.csv");
+        var readCsv = DataReader.CsvReader("prices.csv");
+
+
+
+
+
+
+ DateOnly SkipWeekends(DateOnly d)
+    {
+        while (d.DayOfWeek is DayOfWeek.Saturday || d.DayOfWeek is DayOfWeek.Sunday)
+        {
+            d = d.AddDays(1);
+        }
+
+        return d;
+    }
+
+public record PriceData(string Symbol, DateOnly Date, decimal Open, decimal High, decimal Low, decimal Close, int Volume);
+
+public record LogEntry(int line, string Error, string Reason);
---
+++ csvreader.cs
@@ -0,0 +1,86 @@
+
+
+
+using System.Globalization;
+
+public class DataReader
+{
+    public static List<LogEntry> logs = [];
+
+ public static void ErroReader(int line, string error, string reason)
+  {
+    var log = new LogEntry(line,error, reason);
+    logs.Add(log);
+  }
+  public static List<PriceData> CsvReader (string filePath)
+  {
+
+    var prices = new List<PriceData>();
+
+
+    int failedCount = 0;
+    if (File.Exists(filePath))
+    {
+      var lines = File.ReadLines(filePath)
+                      .Select((line, index) => new{ Text = line , lineNum = index + 1})
+                      .Skip(1);
+
+      foreach(var line in lines)
+      {
+        if(string.IsNullOrEmpty(line.Text))
+        {
+          ErroReader(line.lineNum, line.Text, "The Line is empty");
+          failedCount++;
+          continue;
+        }
+        ;
+        var row = line.Text.Split(",");
+
+        if(row.Length is 7 && DateOnly.TryParse(row[1], CultureInfo.InvariantCulture, out var date) &&
+        decimal.TryParse(row[2], CultureInfo.InvariantCulture, out var open) &&
+        decimal.TryParse(row[3], CultureInfo.InvariantCulture, out var high) &&
+        decimal.TryParse(row[4], CultureInfo.InvariantCulture, out var low) &&
+        decimal.TryParse(row[5], CultureInfo.InvariantCulture, out var close) &&
+        int.TryParse(row[6], CultureInfo.InvariantCulture, out var volume)
+        )
+        {
+
+          bool isValid = !string.IsNullOrWhiteSpace(row[0]) &&
+                          open > 0 && close >0 && high >= low && volume >=0;
+
+          if (isValid)
+          {
+            var priceRecord  = new PriceData(row[0], date, open, high, low, close, volume);
+            prices.Add(priceRecord);
+            Console.WriteLine(row[1]);
+          }
+          else
+          {
+            ErroReader(line.lineNum, "Domain Rule violation", line.Text);
+            failedCount++;
+            continue;
+          }
+        }
+        else
+        {
+          ErroReader(line.lineNum, "Parseing/Column structure erro", line.Text);
+          failedCount++;
+          continue;
+        }
+
+      }
+
+      Console.WriteLine($"Successfully added {prices.Count} and {failedCount} failed!");
+
+
+    }
+    else
+    {
+      ErroReader(0, "No file Found", "No file Found");
+      Console.WriteLine("File does not exists.");
+    }
+    File.WriteAllLines("errors.logs", logs.Select(log => $"Line {log.line}: [{log.Error} -> {log.Reason}]"));
+    return prices;
+  }
+
+}
